e2aef03f8625a01bae07df1e50ed92c6cad17f44
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-07 14:40:29 -0500

    Changes to Landing Page

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,22 @@
+/* eslint-disable prettier/prettier */
 // Material Select Initialization
 // Material Select Initialization
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
-// $(document).ready(function() {
-//   $('select').selectpicker();
+$(document).ready(function () {
+  // Initialize Material Select
+  Waves.attach(".btn", ["waves-effect"]);
+  $(".mdb-select").material_select();
+  $(".material-tooltip-main").tooltip({
+    template: "<div class=\"tooltip md-tooltip\"><div class=\"tooltip-arrow md-arrow\"></div><div class=\"tooltip-inner md-inner\"></div></div>"
+  });
+  $("#modalBtn").click(function (e) {
+    e.preventDefault();
+    $("#myModal").modal();
 
-// });
+
+  });
+
+});
