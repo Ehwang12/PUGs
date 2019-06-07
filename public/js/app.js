7fece75cfaee008c6d5d61839aebf54e40863148
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-07 13:36:09 -0500

    Changes Initial CSS and HTML files

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+// Material Select Initialization
+// Material Select Initialization
+
+window.onbeforeunload = function() {
+  window.scrollTo(0, 0);
+};
+// $(document).ready(function() {
+//   $('select').selectpicker();
+
+// });
