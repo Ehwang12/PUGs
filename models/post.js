2d283db0a05e1bebb4075d91b932b5c55f52edcc
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-10 12:27:04 -0500

    adding changes files

--- models/post.js
+++ models/post.js
@@ -1,7 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
-  var db = sequelize.define("pug_db", {
+  var user = sequelize.define("pug_db", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return db;
+
+  var pickUpGame = sequelize.define("pickUpGame", {
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    address: DataTypes.STRING,
+    StartTime: DataTypes.TIME,
+    city: DataTypes.STRING
+  })
+  return user, pickUpGame;
 };
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,5 @@
 
 module.exports = function(app) {
-<<<<<<< HEAD
-  
-  var db = require("../models");
-  var connection = require("../config/config.json");
-  var path = require('path');
-  //this route will be for when they hit submit on the splash page and are then redirected to the home page
-  app.get('/home', function(res, req){
-    res.sendFile(__dirname,"../public/user.html")
-  });
-  
-  //this  will be the splash page route
-  app.get('/', function(res, req){
-    res.sendFile(path.join(__dirname,"landingPage"))
-=======
   var db = require("../models");
   var connection = require("../config/config.json");
   var path = require('path');
@@ -27,7 +13,6 @@ module.exports = function(app) {
   //this  will be the splash page route
   app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,"../public/landingPage.html"))
->>>>>>> 4cdd2c6f315560a2f8868572d92779ef4c00327d
   });
 
   //this route will post new user information into the database
@@ -35,12 +20,4 @@ module.exports = function(app) {
     var newUser = req.body;
     connection.query('INSERT INTO ')
   });
-<<<<<<< HEAD
-
-  app.post("/newevent", function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/user.html"))
-  })
 };
-=======
-};
->>>>>>> 4cdd2c6f315560a2f8868572d92779ef4c00327d
