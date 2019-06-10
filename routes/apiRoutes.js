18b194e97042c21c4ac8c70efacaeb32313b180f
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-10 10:33:29 -0500

    adding changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,7 @@
 var db = require("../models");
-
+var connection = require('../config')
 module.exports = function(app) {
-  var connection = require('../config')
-
-<<<<<<< HEAD
-  // Create a new example
-  app.post("/api/newevent", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  //app.delete("/api/examples/:id", function(req, res) {
-   // db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-     // res.json(dbExample);
-    //});
-  //});
-=======
-  module.exports = function(app) {
-    
+  
   //this route will be for when they hit submit on the splash page and are then redirected to the home page
   app.get('/home', function(res, req){
     res.sendFile(path.join(__dirname,''))
@@ -28,8 +10,15 @@ module.exports = function(app) {
   //this route will post new user information into the database
   app.post('/user/post', function(req, res){
     var newUser = req.body;
-    connection.query('INSERT INTO ')
-    
+    connection.query('INSERT INTO ') 
   });
->>>>>>> 59438ae74596eecdc8e610279f971d0a8fa9091e
-};
+
+  app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "public\landingPage.html"))
+  });
+
+  //this will post info about the newevent onto the DOM
+  app.post("/newevent", function(res,req) {
+    res.sendFile(path.join(__dirname, ""))
+  })
+  };
