2b26d9e1d8b53f6ba8638bcec953ddcd56ec7ff1
Lorenzo <lgdandrea4@gmail.com>
2019-06-10 12:25:52 -0500

    finished up the API routes and we set the sequels

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,6 +18,20 @@ module.exports = function(app) {
   //this route will post new user information into the database
   app.post('/user/post', function(req, res){
     var newUser = req.body;
-    connection.query('INSERT INTO ')
+    connection.query('INSERT INTO user (name, email, city) values (?, ?, ?)', [newUser.name, newUser.email, newUser.city], function(err, data){
+      if (err) return res.json(500, err);
+      res.json(data);
+    });
   });
+
+  //Will be the new event one
+  app.post('/newevent', function(req, res){
+    var newEvent = req.body;
+    connection.query('INSERT INTO pickUpGame (name, category, address, StartTime, city) values (?, ?, ?, ?,?)', [newEvent.name, newEvent.category,
+    newEvent.address, newEvent.StartTime, newEvent.city], function(err, data){
+      if (err) return res.json(500, err);
+      res.json(data);
+    })
+  })
+
 };
