4a2d5e9940364d2d4d8b858492d4b9f46dc51d24
Lorenzo <lgdandrea4@gmail.com>
2019-06-11 10:33:41 -0500

    yeah buddy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = function(app) {
     
   //this route will be for when they hit submit on the splash page and are then redirected to the home page
-  app.get('/home', function(req, res){
+  app.get('/home/:', function(req, res){
     res.sendFile(path.join(__dirname,"../public/user.html"))
   });
   
@@ -16,16 +16,16 @@ module.exports = function(app) {
   //this route will post new user information into the database
   app.post('/user/post', function(req, res){
     console.log(req.body);
-    db.User.create(req.body).then(function(dbUser){
-      res.json(dbUser);
+    db.User.create(req.body)
+    .then(function(result){
       console.log('completed upload');
-    });
+      res.redirect('/home');   
+    })
   });
 
   //Will be the new event one
   app.post('/newevent', function(req, res){
-    var newEvent = req.body;
-
+    // var newEvent = req.body;
     db.pickUpGame.create(req.body).then(function(dbFunEvent){
       res.json(dbFunEvent);
     });
