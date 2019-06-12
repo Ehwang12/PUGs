124e6a544ce712310b65ba4fd8994c74dac56555
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-12 10:31:19 -0500

    weather widget

--- public/js/weather.js
+++ public/js/weather.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
         location + ",USA&APPID=d1fe11bbbd89325017da7da0cffc0c2b&units=imperial";
         $.getJSON(queryURL,function(result){
-            console.log( JSON.stringify(result.weather, null, 2));
+            console.log( JSON.stringify(result, null, 2));
             var mainWeather = result.weather;
             var currentTemp = result.main.temp;
             var highTemp = result.main.temp_max;
             var lowTemp = result.main.temp_min;
+            var desc = result.weather[0].description;
+            console.log(desc)
 
             var icon= mainWeather[0].icon;
             console.log(icon);
@@ -22,7 +24,7 @@ $(document).ready(function(){
             }    
             var weatherDesc = emptyArr.join('/');
             
-           $("#currentTemp").html('<div class="row"><div class="col-6 align-self-center">' + currentTemp + '°F' + '</div><div class="col-6 align-self-center"><img class="weatherIcon left" src="https://openweathermap.org/img/w/' + icon + '.png"></div></div>');
+           $("#currentTemp").html('<div class="row"><div class="col-md-6 align-self-center">' + currentTemp + '°F' + '</div><div class="col-md-3 align-self-center">' + desc + '</div><div class="col-md-3 align-self-center"><img class="weatherIcon d-inline right w-80" src="https://openweathermap.org/img/w/' + icon + '.png"></div></div>');
 
         weatherDiv.html('<div class="row"><div class="h4 temp col-6 align-self-center"> High: ' + highTemp + '°F' + '</div><div class="col-6 align-self-center h4 temp"> Low: '+ lowTemp + '°F' +'</div></div>');
             // append('<li class="weatherLi list-group-item"></li>');
