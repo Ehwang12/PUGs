a67216da274800f361cd8b9cc3e4e442edb00b85
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-13 09:42:46 -0500

    added del button, footer

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     let aValue = localStorage.getItem('newUser');
     let location = aValue.split(',')[3].split(':')[1].replace(/['"]+/g, '');
-    console.log(location)
     if (location === "Location"){
         location = '55420';
     }
@@ -14,7 +13,6 @@ $(document).ready(function () {
         if (err) {
             location = "55420";
         }
-        console.log(JSON.stringify(result, null, 2))
         var mainWeather = result.weather;
         var currentTemp = Math.round(result.main.temp);
         var highTemp = result.main.temp_max;
@@ -22,8 +20,6 @@ $(document).ready(function () {
         var desc = result.weather[0].description;
         var cityname = result.name;
         var icon = mainWeather[0].icon;
-        console.log(currentTemp);
-
         for (let i = 0; i < mainWeather.length; i++) {
             var weatherDiv = $("#weatherListDiv");
             emptyArr.push(mainWeather[i].main);
